f01dee2cf1774aaef294e15860300a7f4d436694
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-10-05 21:11:44 +0100

    Brought back to basics

--- main.v
+++ main.v
@@ -3,75 +3,25 @@ import ui
 struct App {
 mut:
 	window &ui.Window
-	text string
-}
-
-struct Player {
-mut:
-	username string
-}
-
-struct Players {
-mut:
-	players []&Player
+	label &ui.Label
 }
 
 const (
-	playerWidth = 60
-	playerHeight = 20
-	playerPadding = 5
+	win_width = 600
+	win_height = 800
 )
 
 fn main() {
 
 	mut app := &App{
 		window: 0
+		label: ui.label( text: "Shutt Socket")
 	}
 
-	mut players := &Players{
-		&Player{
-			player.username
-		},
-		&Player {
-			player.username
-		}
-	}
-
-	children = [
-		ui.textbox(
-			text: &app.text
-			placeholder: '0'
-			width: 135
-		),
-	]
-
-	for player in players {
-		children << ui.row(
-			spacing: playerPadding,
-			height: playerHeight,
-			widths: ui.stretch
-
-		)
-	}
-
-	ui.window(
-		width: 600,
-		height: 800,
+	app.window = ui.window(
+		width: win_width,
+		height: win_height,
 		title: "Shutt Socket",
-		children: [
-			ui.textbox(
-				max_len: 20
-				width: 200
-				placeholder: 'First name'
-				text: &app.first_name
-			),
-			ui.textbox(
-				max_len: 50
-				width: 200
-				placeholder: 'Last name'
-				text: &app.last_name
-			)
-		]
 	)
 
 	ui.run(app.window)
